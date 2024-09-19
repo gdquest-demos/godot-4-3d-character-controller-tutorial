51865fe59bce4f05f2b163e586ad5a317c9e1836
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-09-19 10:47:48 +0200

    rename files, format GDScript files

--- lesson_reference/player_3d_010_camera.gd
+++ lesson_reference/player_3d_010_camera.gd
@@ -10,6 +10,7 @@ var _camera_input_direction := Vector2.ZERO
 @onready var _camera_pivot: Node3D = %CameraPivot
 @onready var _camera: Camera3D = %Camera3D
 
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_fullscreen"):
 		get_viewport().mode = Window.MODE_FULLSCREEN
@@ -18,6 +19,7 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_pressed("left_click"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+
 func _unhandled_input(event: InputEvent) -> void:
 	var is_camera_motion := (
 		event is InputEventMouseMotion and
@@ -25,6 +27,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	)
 	if is_camera_motion:
 		_camera_input_direction = event.screen_relative * mouse_sensitivity
+
 
 func _physics_process(delta: float) -> void:
 	_camera_pivot.rotation.x += _camera_input_direction.y * delta
